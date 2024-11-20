cd01e22c36751e4e5eaa393eda28bec764aa8140
Lucas4680 <27tanglucas@gmail.com>
2024-11-19 21:33:03 -0800

    Revamp_website

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,16 +6,20 @@ const lexend = Lexend({  weight: ['400'], subsets: ["latin"] });
 
 const Navbar = () => {
     return (
-      <div className={`${styles.topnav} ${lexend.Lexend}`}>
-        {/* <a>
-          <img href="/" src="Hack4All-centered.png" alt="Home" className={styles.topnav_img}></img>
-        </a> */}
-        <a href="/"><img href="/" src="Hack4All Logo Blue-centered.png" alt="Home" className={styles.topnav_img}></img></a>
-        <b>
-          <a href="https://hcb.hackclub.com/donations/start/hack4all" className={styles.topnav_item}>Donate</a>
-          <a href="/events" className={styles.topnav_item}>Events</a>
-          <a href="/contact" className={styles.topnav_item}>Contact Us</a>
-        </b>
+      <div>
+        <div className={`${styles.topnav} ${lexend.Lexend}`}>
+          {/* <a>
+            <img href="/" src="Hack4All-centered.png" alt="Home" className={styles.topnav_img}></img>
+          </a> */}
+          <a href="/"><img href="/" src="Hack4All-centered.png" alt="Home" className={`${styles.topnav_img} ${styles.hvr}`}></img></a>
+          <div>
+            <a href="/contact" className={styles.topnav_item}>Contact Us</a>
+            <a href="/events" className={styles.topnav_item}>Events</a>
+            <a href="https://hcb.hackclub.com/donations/start/hack4all" className={styles.topnav_item}>Donate</a>
+          </div>
+        </div>
+      <div>
+      </div>
       </div>
       );
 };
